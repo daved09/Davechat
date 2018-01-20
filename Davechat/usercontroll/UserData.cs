d415943662c30f6e39af59aeeca76d1648b94811
daved09 <daluba@live.de>
2018-01-20 15:07:55 +0100

    Userdata added

---
+++ Davechat/usercontroll/UserData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Davechat.usercontroll
+{
+    class UserData
+    {
+
+        public UserData() : this("User")
+        {
+        }
+
+        public UserData(string UserName)
+        {
+            this.UserName = UserName;
+            Initalize();
+        }
+    
+        public string UserName { get; set; }
+        private List<String> Messages;
+
+        //initalizer
+        private void Initalize()
+        {
+            Messages = new List<string>();
+        }
+
+
+    }
+}
